4abe3da01b300b8f9233d8a23bd6de6f01e67e87
João Paulo Marcuz <126777006+Marcuz015@users.noreply.github.com>
2024-05-20 14:32:13 -0300

    Api completa

--- index.js
+++ index.js
@@ -69,6 +69,51 @@ app.post('/celulares',async(req,res)=>{
     }
 })
 
+
+
+app.get('/user', async (req, res) => {
+    try {
+        let nome = req.query.nome;
+        let result;
+
+        // Consultar o banco de dados
+        if (nome) {
+            result = await celulares.consultarUser(nome);
+        } else {
+            result = await celulares.consultarUser();
+        }
+
+        // Verificar se há resultados
+        if (result.length > 0) {
+            res.status(200).json(result);
+        } else {
+            // Se nenhum resultado for encontrado, retornar 404
+            res.status(404).json({ erro: 'Nenhum recurso encontrado' });
+        }
+    } catch (error) {
+        // Lidar com erros de forma adequada, por exemplo, retornando um status 500
+        console.error('Erro na consulta:', error);
+        res.status(500).json({ erro: 'Erro interno do servidor' });
+    }
+});
+
+
+app.post('/cadastrar',async(req,res)=>{
+    try {
+        const { nome, senha, gmail } = req.body; // Supondo que os dados enviados tenham campos 'nome' e 'marcaDeMercado'
+
+        // Chamar a função cadastrar com os dados recebidos
+        const novouser = await celulares.cadastrarUser(nome, senha, gmail);
+
+        // Enviar uma resposta com os dados da nova celulares cadastrada
+        res.status(201).json(novouser);
+    } catch (error) {
+        // Lidar com erros, se houver algum
+        console.error('Erro ao cadastrar celulares:', error);
+        res.status(500).json({ erro: 'Erro interno do servidor' });
+    }
+})
+
 app.listen(3000,()=>{
     let data = new Date();
     console.log(`Sistema inicializado: \nInf:${data}`);
